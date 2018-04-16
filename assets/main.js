5c9eb33c9cae1c99a1fb74bc788940fc9f5e0dac
Ichigo <x564458193@gmail.com>
2018-04-17 04:39:02 +0800

    test

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,49 @@
 
-let div = document.querySelector("#content")
-fetch('./docs/test.md')
-    .then(function (response) {
-        return response.text();
+
+function initCategories(categories) {
+    const nav = document.querySelector(".menu")
+    categories.sort((a, b) => {
+        return a.name > b.name
     })
-    .then(function(txt){
-        console.log(txt);
-        div.innerHTML = marked(txt)
-    })
+    categories.forEach(
+        category => {
+            const a = document.createElement("a")
+            a.className = "menu-item"
+            a.href = "#"
+            a.innerHTML = category.name
+            nav.appendChild(a)
+        }
+    )
+}
+
+function loadArticles(articles) {
+    const content = document.querySelector("#content")
+    articles.forEach(
+        article => {
+            const div = document.createElement("div")
+            fetch(article.path).then(response => {
+                return response.text()
+            }).then(text => {
+                div.innerHTML = marked(text)
+                content.appendChild(div)
+            })
+        }
+    )
+}
+
+function main() {
+    const content = document.querySelector("#content")
+    fetch('config.json')
+        .then(response => {
+            return response.json()
+        })
+        .then(config => {
+            console.log(config)
+            initCategories(config.categories)
+            return config
+        }).then(config => {
+            loadArticles(config.articles)
+        })
+}
+
+main()
